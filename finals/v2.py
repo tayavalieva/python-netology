6bff75e5a5fa537722df9e3fce92b5b04cd14b85
Taya <taya.skachkova@gmail.com>
2017-03-07 23:17:23 +0800

    Final Project

--- finals/v2.py
+++ finals/v2.py
@@ -3,7 +3,6 @@ from requests import ReadTimeout
 from vk.exceptions import VkAPIError
 import pandas as pd
 
-
 user_id = ''
 app_id = ''
 login = ''
@@ -28,9 +27,10 @@ def get_followers_list(user_id):
 
 def get_fans_list(user_id):
     followers_list = get_followers_list(user_id)
-    friends_list = api.friends.get(user_ids = user_id)
+    friends_list = api.friends.get(user_ids=user_id)
     fans_list = followers_list + friends_list
     return fans_list
+
 
 def get_fans_groups_dict(fans_list):
     success_count = 0
@@ -38,7 +38,7 @@ def get_fans_groups_dict(fans_list):
     fans_groups_dict = {}
     for fan in fans_list:
         try:
-            groups_list = api.groups.get(user_id = fan)
+            groups_list = api.groups.get(user_id=fan)
             success_count += 1
             for group in groups_list:
                 if group in fans_groups_dict:
@@ -49,16 +49,18 @@ def get_fans_groups_dict(fans_list):
             fail_count += 1
         except ReadTimeout as e:
             fail_count += 1
-            #print('При обработке пользователя с ID {} возникла ошибка: {}.'.format(fan, e))
-    print('{} ID обработаны успешно.{} ID не удалось обработать.'.format(success_count,fail_count))
+            # print('При обработке пользователя с ID {} возникла ошибка: {}.'.format(fan, e))
+    print('{} ID обработаны успешно.{} ID не удалось обработать.'.format(success_count, fail_count))
     return fans_groups_dict
+
 
 def get_group_name(group_id):
     try:
-        group_name = api.groups.getById(group_ids = group_id)[0]['name']
+        group_name = api.groups.getById(group_ids=group_id)[0]['name']
         return group_name
     except ReadTimeout as e:
         return ""
+
 
 def create_dataframe(fans_groups_dict):
     df = pd.DataFrame(list(fans_groups_dict.items()), columns=['GroupID', 'FansNumber'])
@@ -66,17 +68,11 @@ def create_dataframe(fans_groups_dict):
     df['GroupName'] = df['GroupID'].map(get_group_name)
     return df
 
+
 df = create_dataframe(get_fans_groups_dict(get_fans_list(user_id)))
 df.to_json("groups.json")
 
 
-#df.to_csv('groups.csv', encoding="utf-8")
+# df.to_csv('groups.csv', encoding="utf-8")
 
-#df = pd.read_csv("groups.csv")
-
-
-
-
-
-
-
+# df = pd.read_csv("groups.csv")
